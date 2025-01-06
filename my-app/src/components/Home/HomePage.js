f608efae771478f1be77418f0aaf1e139677a2c6
thanhhuhu <nguyendinhthanh2002@gmail.com>
2025-01-06 23:49:51 +0300

    6/1

--- my-app/src/components/Home/HomePage.js
+++ my-app/src/components/Home/HomePage.js
@@ -1,6 +1,13 @@
-const HomePage = () => {
+import React from "react";
+import videoHomepage from "../../assets/video-homepage.mp4";
+const HomePage = (props) => {
     return (
-        <div className="homepage-container">Home page</div>
+        <div className="homepage-container">
+            <video autoPlay={true} muted loop >
+                <source src={videoHomepage}
+                        type="video/mp4" />
+            </video>
+        </div>
     )
 }
 export default HomePage;
