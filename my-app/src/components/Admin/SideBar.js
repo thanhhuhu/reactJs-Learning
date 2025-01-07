c29668f3585bff2006bd00f690664064033f19da
thanhhuhu <nguyendinhthanh2002@gmail.com>
2025-01-07 19:27:16 +0300

    7/1

---
+++ my-app/src/components/Admin/SideBar.js
@@ -0,0 +1,97 @@
+import 'react-pro-sidebar/dist/css/styles.css';
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import sidebarBg from '../../assets/bg2.jpg';
+import { IoLogoReact } from "react-icons/io5";
+import{MdDashboard}  from 'react-icons/md';
+import{DiReact} from 'react-icons/di';
+import './SideBar.scss'
+const SideBar = (props) => {
+    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <DiReact size ={'3em'} color={"blue"} />
+                        <span>Thanh</span>
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<FaTachometerAlt />}
+                            // suffix={<span className="badge red">New</span>}
+                        >
+                            Dashboard
+                        </MenuItem>
+                        {/*<MenuItem icon={<FaGem />}> components </MenuItem>*/}
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            icon={<FaGem/>}
+                            title={"Features"}
+                            // suffix={<span className="badge yellow">3</span>}
+                            // icon={<FaRegLaughWink />}
+                        >
+                            <MenuItem> Quản lý User </MenuItem>
+                            <MenuItem> Quản lý User </MenuItem>
+                            <MenuItem> Quản lý User </MenuItem>
+                        </SubMenu>
+
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            href="https://github.com/thanhhuhu"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                view my github page
+                            </span>
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBar;
--- my-app/src/components/Home/HomePage.js
+++ my-app/src/components/Home/HomePage.js
@@ -7,6 +7,13 @@ const HomePage = (props) => {
                 <source src={videoHomepage}
                         type="video/mp4" />
             </video>
+            <div className="homepage-content">
+                <div className={"title-1"}>Get to know your customers with forms worth filling out</div>
+                <div className={"title-2"}>Collect all the data you need to understand customers with forms designed to be refreshingly different.</div>
+                <div className={"title-3"}>
+                    <button>Get's started. It's free</button>
+                </div>
+            </div>
         </div>
     )
 }
