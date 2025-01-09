f1dcdeb3ea841163ae7573fa94f32810e609e4fc
thanhhuhu <nguyendinhthanh2002@gmail.com>
2025-01-09 17:22:35 +0300

    9/1

--- my-app/src/components/Admin/Content/ManangeUser.js
+++ my-app/src/components/Admin/Content/ManangeUser.js
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ModalCreateUser from "./ModalCreatorUser";
+
 const ManageUser =(props) =>{
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
             <div className={"manage-user-container"}>
                 <div className={"title"}>
@@ -8,7 +18,7 @@ const ManageUser =(props) =>{
                     <button> Add new user</button>
                 </div>
                 <div>
-                    Table user
+                    <ModalCreateUser></ModalCreateUser>
                 </div>
             </div>
     )
